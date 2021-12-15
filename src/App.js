d5c7309f354854c64f89a4d5c2719d4463a8d6b7
Kuntash <kuntashtashi11@gmail.com>
2021-12-15 23:20:51 +0530

    Top nav starter files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import TopNav from './Components/TopNav/TopNav';
 import './App.css';
 
 function App() {
-    return <div className="App">Hello</div>;
+    return (
+        <div className="App">
+            <TopNav />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Components/TopNav/TopNav.js
@@ -0,0 +1,10 @@
+import './TopNav.css';
+const TopNav = () => {
+    return (
+        <div className="navbar">
+            <h1>TopNav</h1>
+        </div>
+    );
+};
+
+export default TopNav;
