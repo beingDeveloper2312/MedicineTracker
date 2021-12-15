13bb01492b096fa4b3c606f93848b3cc3699f49e
Raunit Verma <ramgopal10258@gmail.com>
2021-12-15 20:19:22 +0530

    Adding AddNew with clock timer

--- app/src/main/java/com/shyptsolution/medicinetracker/add/AddNew.kt
+++ app/src/main/java/com/shyptsolution/medicinetracker/add/AddNew.kt
@@ -1,27 +1,49 @@
 package com.shyptsolution.medicinetracker.add
 
-import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.TimePicker
+import androidx.fragment.app.FragmentManager
 import com.shyptsolution.medicinetracker.R
-import java.nio.channels.SelectableChannel
 
 class AddNew : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_new)
-        var button=findViewById<Button>(R.id.button)
+        var button=findViewById<Button>(R.id.settime)
         button.setOnClickListener {
-            SelectTime()
+            selectTime()
         }
     }
 
-    fun SelectTime(){
-        var clocktime=findViewById<TimePicker>(R.id.timePicker)
-        clocktime.visibility= View.VISIBLE
+    private fun selectTime() {
+        val popTime=PopUpTime()
+        var fgm=supportFragmentManager
+        popTime.show(fgm,"fdsf")
+    }
+
+    fun setTime(Hours:Int,Minutes:Int){
+        var time=findViewById<TextView>(R.id.time)
+        if(Minutes==0 && Hours==0){
+            time.setText("00:00")
+
+        }
+       else if (Hours==0){
+            time.setText("00"+":"+Minutes.toString())
+
+        }
+        else if(Minutes==0){
+            time.setText(Hours.toString()+":00")
+
+        }
+
+        else{
+            time.setText(Hours.toString()+":"+Minutes.toString())
+
+        }
     }
 
 }
---
+++ app/src/main/java/com/shyptsolution/medicinetracker/add/PopUpTime.kt
@@ -0,0 +1,35 @@
+package com.shyptsolution.medicinetracker.add
+
+import android.os.Build
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TimePicker
+import androidx.fragment.app.DialogFragment
+import com.shyptsolution.medicinetracker.MainActivity
+import com.shyptsolution.medicinetracker.R
+
+class PopUpTime:DialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        var view=inflater.inflate(R.layout.popuptime,container,false)
+        var setTime=view.findViewById(R.id.setTime) as Button
+        var timepicker=view.findViewById(R.id.timePicker) as TimePicker
+        setTime.setOnClickListener {
+
+            val MainAct=activity as AddNew
+            if(Build.VERSION.SDK_INT>=23) {
+                MainAct.setTime(timepicker.hour, timepicker.minute)
+            }
+            this.dismiss()
+            }
+        return view
+    }
+
+}
