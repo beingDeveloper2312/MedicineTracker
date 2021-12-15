4e2737f2edf1fdde854527da21bfd6a49f58d7fc
Raunit Verma <ramgopal10258@gmail.com>
2021-12-15 17:25:57 +0530

    Adding AddNew Task & added back button close feature

--- app/src/main/java/com/shyptsolution/medicinetracker/MainActivity.kt
+++ app/src/main/java/com/shyptsolution/medicinetracker/MainActivity.kt
@@ -1,23 +1,45 @@
 package com.shyptsolution.medicinetracker
 
+import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
+import android.transition.Slide
+import android.transition.TransitionManager
+import android.view.*
+import android.widget.*
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.RecyclerView
+import com.shyptsolution.medicinetracker.Login.Login
 import com.shyptsolution.medicinetracker.add.AddNew
 
 class MainActivity : AppCompatActivity() {
+    private var backPressedTime=0L
+    private var searchClicked=false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    override fun onBackPressed() {
-//        super.onBackPressed()
-    }
 
+    override fun onBackPressed() {
+        if (searchClicked){
+            super.onBackPressed()
+            searchClicked=false
+        }
+         if(backPressedTime+2000>System.currentTimeMillis()){
+            super.onBackPressed()
+            finishAffinity()
+        }
+        else{
+            Toast.makeText(this,"Press Back Again To Exit",Toast.LENGTH_SHORT).show()
+        }
+        backPressedTime=System.currentTimeMillis()
+
+    }
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater: MenuInflater =menuInflater
         menuInflater.inflate(R.menu.home_menu,menu)
@@ -31,9 +53,16 @@ class MainActivity : AppCompatActivity() {
                     var intent = Intent(this, AddNew::class.java)
                     this.startActivity(intent)
                 }
+                R.id.app_bar_search->{
+                    searchClicked=true
+                }
             }
         }
 
         return super.onOptionsItemSelected(item)
     }
+
+
+
+
 }
--- app/src/main/java/com/shyptsolution/medicinetracker/add/AddNew.kt
+++ app/src/main/java/com/shyptsolution/medicinetracker/add/AddNew.kt
@@ -1,12 +1,27 @@
 package com.shyptsolution.medicinetracker.add
 
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TimePicker
 import com.shyptsolution.medicinetracker.R
+import java.nio.channels.SelectableChannel
 
 class AddNew : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_new)
+        var button=findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            SelectTime()
+        }
     }
+
+    fun SelectTime(){
+        var clocktime=findViewById<TimePicker>(R.id.timePicker)
+        clocktime.visibility= View.VISIBLE
+    }
+
 }
