cf6eb371756bda849e275f48baf1c0b464157a81
Kuntash <kuntashtashi11@gmail.com>
2021-12-17 00:49:30 +0530

    desktop version ready, responsive yet to be finished

--- src/Components/TopNav/TopNav.js
+++ src/Components/TopNav/TopNav.js
@@ -1,8 +1,24 @@
 import './TopNav.css';
 const TopNav = () => {
+    const navItems = [
+        { title: 'Home', href: '#' },
+        { title: 'Find a Doctor', href: '#' },
+        { title: 'Apps', href: '#' },
+        { title: 'Testimonials', href: '#' },
+        { title: 'About us', href: '#' },
+    ];
     return (
         <div className="navbar">
-            <h1>TopNav</h1>
+            <div className="navbar__logo">M</div>
+            <div className="navbar__link--container">
+                {navItems.map((item, i) => (
+                    <div key={i} className="navbar__item">
+                        <a className="navbar__link" href={item.href}>
+                            {item.title}
+                        </a>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
