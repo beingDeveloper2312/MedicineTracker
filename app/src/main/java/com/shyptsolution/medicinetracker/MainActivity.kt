ac599ff5c7681fd6b6a57de68f5350cd87b89417
Raunit Verma <ramgopal10258@gmail.com>
2021-12-17 03:08:17 +0530

    added nav bar and changes in the Addnew

--- app/src/main/java/com/shyptsolution/medicinetracker/MainActivity.kt
+++ app/src/main/java/com/shyptsolution/medicinetracker/MainActivity.kt
@@ -6,25 +6,41 @@ import android.graphics.Color
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.system.Os.close
 import android.transition.Slide
 import android.transition.TransitionManager
 import android.view.*
 import android.widget.*
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.navigation.NavigationView
 import com.shyptsolution.medicinetracker.Login.Login
 import com.shyptsolution.medicinetracker.add.AddNew
+import java.lang.IllegalStateException
 
 class MainActivity : AppCompatActivity() {
     private var backPressedTime=0L
     private var searchClicked=false
-
+    lateinit var toggle: ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var driver: DrawerLayout =findViewById(R.id.mainactivity)
+        toggle= ActionBarDrawerToggle(this,driver,R.string.open,R.string.close)
+        driver.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        var floatingActionButton=findViewById<FloatingActionButton>(R.id.floatingActionButton)
+        floatingActionButton.setOnClickListener{
+            startActivity(Intent(this,AddNew::class.java))
+        }
     }
 
-
+    //BAck button close function
     override fun onBackPressed() {
         if (searchClicked){
             super.onBackPressed()
@@ -40,13 +56,18 @@ class MainActivity : AppCompatActivity() {
         backPressedTime=System.currentTimeMillis()
 
     }
+    //Adding option in menu
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater: MenuInflater =menuInflater
         menuInflater.inflate(R.menu.home_menu,menu)
         return super.onCreateOptionsMenu(menu)
     }
-
+    //adding click function on navigation bar
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)){
+            return true
+        }
+
         if(item!=null){
             when(item.itemId){
                 R.id.addnew-> {
